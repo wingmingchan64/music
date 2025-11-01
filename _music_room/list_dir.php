ede0875c9dc783dd1dc79c888bc2cbbc6fb63668
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-11-01 11:14:18 -0500

    Updated

---
+++ _music_room/list_dir.php
@@ -0,0 +1,47 @@
+<?php
+/*
+php h:\github\music\_music_room\list_dir.php
+*/
+
+$par_path = 'e:';
+$dir_name = 'Collections';
+
+list_dir( $par_path, $dir_name, '  ' );
+
+function list_dir(
+	string $par_path,
+	string $dir_name, 
+	string $padding )
+{
+	$dir_path = $par_path . "\\" . $dir_name;
+	
+	if( is_dir( $dir_path ) )
+	{
+		$contents = scandir( $dir_path );
+
+		echo "${padding}${dir_name}\n";
+		
+		foreach( $contents as $item )
+		{
+			// Exclude "." and ".." which represent the current and parent directories
+			if( $item != '.' && $item != '..')
+			{
+				if( is_dir( 
+					$dir_path . "\\" . $item ) )
+				{
+					list_dir( $dir_path, $item, $padding . "  " );
+				}
+				else
+				{
+					//echo $item . "\n";
+				}
+			}
+		}
+	}
+	else
+	{
+		echo "Error: Directory '$dir_path' not found or not readable.\n";
+	}
+}
+?>
+
